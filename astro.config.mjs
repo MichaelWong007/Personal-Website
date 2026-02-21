feb06e63d92e3dbf4c0f1fcd7fb06d19715d6c09
重阳 王 <MichaelWong007@126.com>
2026-02-22 01:01:14 +0800

    再次调整 API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/server';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [mdx()],
