82bce6f0b0ce209fd2c987e7dafc1f48c9b03063
重阳 王 <MichaelWong007@126.com>
2026-02-22 18:09:30 +0800

    版本 Epitaph 碑文 正式上线

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [mdx()],
-  output: 'static',
-  adapter: vercel()
+  output: 'static'
 });
